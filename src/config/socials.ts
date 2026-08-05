c004262160b6fb59661a3fbfeb53c179516a73fb
Maksim Nagaev <alma.ai.research@gmail.com>
2026-08-05 16:09:21 -0400

    Centralize social links

---
+++ src/config/socials.ts
@@ -0,0 +1,12 @@
+export const SOCIALS = {
+  github: "https://github.com/MaksimNagaevEthicalAI",
+
+  medium: "https://medium.com/@maksimnagaev",
+
+  linkedin: "https://www.linkedin.com/in/maksim-nagaev-9a03013a8/",
+
+  substack: "https://substack.com/@maksimnagaev",
+
+  notion:
+    "https://button-alyssum-638.notion.site/ALMA-AI-32a45b82b970803984cfd20456a8f252",
+};
